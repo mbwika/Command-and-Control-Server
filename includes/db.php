c7049f9cdfd1012622901e42cf9b2bb2578b161d
Sir Mbwika <collinsmbwika10@gmail.com>
2025-04-15 22:51:35 -0500

    Update db.php
    Removed real db credentials

--- includes/db.php
+++ includes/db.php
@@ -3,9 +3,9 @@
     <?php
         // Database constants
         define('DB_HOST', '127.0.0.1');
-        define('DB_USER', 'root'); //smartkitty
-        define('DB_PASSWORD', ''); // sMRrTcAt@2
-        define('DB_DATABASE', 'backup_db'); // u446905009_intel_db
+        define('DB_USER', 'root'); 
+        define('DB_PASSWORD', ''); 
+        define('DB_DATABASE', 'backup_db'); 
 
     class Database {
         function getDbConnection() {
